aeeb348ce62f5ad0c06d09f1ad1adb12647e2bb1
mayur <mayur@gmail.com>
2024-03-02 06:49:01 +0530

    File

--- mayur.tf
+++ mayur.tf
@@ -168,3 +168,170 @@ variable "project" {
 output "public_ip" {
     value = aws_instance.pub-instance.public_ip
 }
+
+
+terraform {
+    backend "s3" {
+        bucket = my-s3-bucket 
+        key = terraform.tfstate
+        region = ap-south-1
+    }
+}
+provider "aws" {
+    region = ap-south-1
+}
+resource "aws_instnace" "my-ec2" {
+    ami = var.ami 
+    instance_type = t2.micro
+    key_pair = id_rsa 
+    vpc_security_group_id = ["place your vpc here"]
+}
+
+variable "key_pair" {
+    default = id_rsa
+}
+variable "instance_type" {
+    default = t2.micro 
+}
+variable "instance_id" {
+    default = ahughrgr
+}
+variable "tags" {
+    default = {
+        type = map
+        name = my-instance
+        env  = devops
+    }
+}
+
+terraform {
+    backend "s3" {
+        bucket = my-s3-bucket 
+        key = terraform.tfstate 
+        region = ap-south-1
+    }
+}
+provider "aws" {
+    region = ap-south-1
+}
+
+resource "aws_vpc" "my-vpc" {
+    cidr_block = var.vpc-cidr 
+    tags = {
+        name = "${var.project}-vpc"
+    }
+}
+resource "aws_subnet" "pub-subnet" {
+    vpc_id = aws_vpc.my-vpc.id 
+    cidr_block = var.pub-cidr 
+    availability_zone = var.pub-az
+    tags = {
+       name = "${var.project}"
+    }
+    map_public_ip_on_launch = true
+}
+resource "aws_subnet" "pvt-subnet" {
+    vpc_id = aws_vpc.my-vpc.id 
+    cidr_block = var.pvt-cidr 
+    tags = {
+        name = "${var.project}-pvt"
+    }
+    availability_zone = var.pvt-az
+}
+resource "aws_internet_gateway" "my-igw" {
+    vpc_id = aws_vpc.my-vpc.id 
+    tags = {
+        name = "${var.project}"
+    }
+}
+
+resource "aws_default_route_table" "my-rt" {
+    default_route_table_id = aws_vpc.my-vpc.default_route_table_id
+
+    route {
+        cidr_block = ["0.0.0.0/0"]
+        gateway_id = aws_internet_gateway.my-igw.id
+    }
+    tags = {
+        name = "${var.project}-myroute"
+    } 
+}
+
+resource "aws_security_group" "my-sg" {
+    name = "${var.project}-sg"
+    description = all httpd port allow 
+    vpc_id = aws_vpc.my-vpc.id 
+
+    ingress {
+        from_port = 80
+        to_port = 80
+        protocol = TCP 
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = TCP 
+        cidr_blocks = [0.0.0.0/0]
+    }
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = -1
+        cidr_blocks = [0.0.0.0/0]
+    }
+
+    tags = {
+        name = "${var.project}-sg"
+    }
+}
+resource "aws_instance" "pub-instace" {
+    ami = var.ami 
+    instance_type = var.instance_type 
+    key_name = var.key_pair
+    subnet_id = aws_subnet.pub-subnet.id 
+    vpc_security_group_ids = [aws_security_grop.my-sg.id]
+    
+    user_data = << EOF
+    #!/bin/bash
+    yum update -y
+    yum install httpd -y
+    systemctl start httpd.service
+    echo "hello" > /var/www/html/index.html
+    systemctl restart httpd.service
+    systemctl status httpd.service
+    systemctl enable httpd.service
+    EOF
+
+    depends_on = [ aws_secuiry_grop.my-sg ]
+}
+resource "aws_instance" "pvt-instance" {
+    ami = var.ami
+    instance_type = var.instance_type
+    key_name = var.key_pair 
+    subnet_id = aws_subnet.pvt-subnet.id 
+    vpc_security_group_ids = aws_security_group.my-sg.id 
+}
+
+output "public_ip" {
+    value = aws_isntance.pub-instance.id 
+}
+
+variable "vpc-cidr" {
+    default = 10.0.0.0/16
+}
+variable "pub-cidr" {
+    default = 10.0.1.0/24
+}
+variable "pvt-cidr" {
+    default = 10.0.2.0/24
+}
+variable "pub-az" {
+    default = ap-souht-1a
+}
+variable "pvt-az" {
+    default = ap-south-1b
+}
+variable "project" {
+    default = cloudblitz
+}
